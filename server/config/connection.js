f244c70314581cbb81e821a80668f2c1696a8ebe
Nicholas Skinner <njskinner@ucdavis.edu>
2021-12-14 12:38:03 -0800

    attempting to deploy to heroku. adding in mongoose dependencies

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/rise-app',
+  process.env.MONGODB_URI || 'mongodb+srv://njskinner@ucdavis.edu:HSQcFmjC!Cp3HrFK@<cluster>/RiseApp?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
   }
-);
+)
+  .then(() => console.log("Database Connected Successfully"))
+  .catch(err => console.log(err));
 
 module.exports = mongoose.connection;
+
+// const { MongoClient } = require("mongodb");
+ 
+// // Replace the following with your Atlas connection string                                                                                                                                        
+// const url = "mongodb+srv://IGuessImNick:HSQcFmjC%!Cp3HrFK%@riseapp.hmdol.mongodb.net/RiseApp?retryWrites=true&w=majority";
+// const client = new MongoClient(url);
+
+// async function run() {
+//     try {
+//         await client.connect();
+//         console.log("Connected correctly to server");
+
+//     } catch (err) {
+//         console.log(err.stack);
+//     }
+//     finally {
+//         await client.close();
+//     }
+// }
+
+// run().catch(console.dir);
